23865b41f7df36686cf3bbd52f2ddbf2232088b4
Nurgazy <amanbekovnurgazy@mail.ru>
2022-12-27 12:53:08 +0600

    first commit

---
+++ main/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Task(models.Model):
+    title = models.CharField(max_length=40)
+    body = models.TextField(blank=True)
+    deadline = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return  self.title
+
